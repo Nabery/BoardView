3f54cee7b11c77752c84e5981ae54974f10a383f
NabNabery <paulosouzadev@outlook.com>
2022-07-18 11:08:50 -0300

    1.0

--- components/button.js
+++ components/button.js
@@ -5,7 +5,7 @@ function button({getData}) {
   return (
     <div>
         <button onClick= {()=> getData('https://getpantry.cloud/apiv1/pantry/0e12a1da-6eeb-4bd9-985c-28217a25f86c/basket/finaldatajson')}
-         className = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-20 px-40 rounded-full'>
+         className = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
          GET INFO
         </button>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
   const [list, setList] = useState(0);
 
 
-
   async function getData(url) {
     const response = await fetch(
       url,
@@ -14,14 +13,22 @@ export default function Home() {
         method: 'GET',
       }
     );
-    const data = await response.json();
-    setList(data.data.map(board=> ({OUT: board.name, Links: board.items.map(item=> ({Reporter: item.title, Retorno: item.feedback}))})))
-    console.log(list)
+    setList(await response.json());
+    Object.keys(list).length > 0 ?
+    console.log(list.data.map(board=> ({nome: board.name, items: board.items.map(item=>({nome: item.title, feedback: item.feedback}))})))
+    : {}
   }
 
   return (
+  <div>
+
     <div className="grid grid-cols-6 gap-1 my-5">
-      <Button getData={getData} />
+     {Object.keys(list).length > 0 ? list.data.map(board => { return (<div className = '' key = {board.name}>
+      <h1 className = 'text-red-500'>{board.name}</h1>
+      {board.items.map(item => {return (<Card key = {item.title} name = {item.title} feedback = {item.feedback}/>)})}
+      </div>)}) : console.log('Waiting for button press')}
     </div>
+    <Button getData={getData} />
+  </div>
   )
 }
